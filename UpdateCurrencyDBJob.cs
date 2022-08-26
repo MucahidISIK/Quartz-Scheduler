be2fcaa967bdffa0658a23044c1ee7fd3df6cc6c
Mücahid IŞIK <56644702+MucahidISIK@users.noreply.github.com>
2022-08-26 14:15:25 +0300

    Add files via upload

---
+++ UpdateCurrencyDBJob.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+
+namespace UpdateScheduler
+{
+    public class UpdateCurrencyDBJob : IJob
+    {
+        Task IJob.Execute(IJobExecutionContext context)
+        {
+            using (CurrencyDBService.CurrencyDBServiceClient serviceClient = new CurrencyDBService.CurrencyDBServiceClient())
+            {
+                string msg = serviceClient.updateDBAsync().Result;
+                return Task.FromResult(msg);
+            }
+        }
+    }
+}
---
+++ UpdateCurrencyDBJobScheduler.cs
@@ -0,0 +1,27 @@
+﻿using Quartz;
+using Quartz.Impl;
+
+namespace UpdateScheduler
+{
+    public class UpdateCurrencyDBJobScheduler
+    {
+        public static void Start()
+        {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler().Result;
+            scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<UpdateCurrencyDBJob>().Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithDailyTimeIntervalSchedule
+                  (s =>
+                     s
+                    .OnEveryDay() //hergün çalışacağı bilgisi
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(11,56)) //hergün hangi saatte çalışacağı bilgisi
+                  )
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
